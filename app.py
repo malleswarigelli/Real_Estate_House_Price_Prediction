3be2f8bb60ede0c3ce482ead68bf34205a91917e
Malli Gelli <malleswari.gelli@gmail.com>
2023-02-11 10:58:50 -0600

    Added Docker and app.py files

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
-@app.rout("/", methods = ["GET", "POST"])
+@app.route("/", methods = ["GET", "POST"])
 def index():
     return "start ML application"
 
