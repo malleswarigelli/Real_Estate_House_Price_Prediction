fc4de4442e695611b7d4402d6666c82207680315
Malli Gelli <malleswari.gelli@gmail.com>
2023-02-12 11:10:10 -0600

    artifact_entity, config_entity are updated

---
+++ housing/entity/artifact_entity.py
@@ -0,0 +1,27 @@
+from collections import namedtuple
+
+DataIngestionArtifact = namedtuple("DataIngestionArtifact",
+                                   ["train_file_path",
+                                    "test_file_path","is_ingested",
+                                    "message"])
+
+DataValidationArtifact = namedtuple("DataValidationArtifact",
+                ["schema_file_path","report_file_path",
+                                   "report_page_file_path","is_validated",
+                                   "message"])
+
+DataTransformationArtifact = namedtuple("DataTransformationArtifact",
+                ["transformed_train_file_path","transformed_test_file_path",
+                  "preprocessed_object_file_path","is_transformed","message"])
+
+ModelTrainerArtifact = namedtuple("ModelTrainerArtifact",
+                                  ["trained_model_file_path","is_model_trained","message",
+                                   "train_rmse", "test_rmse",
+                                   "train_accuracy","test_accuracy",
+                                   "model_accuracy"])
+
+ModelEvaluationArtifact = namedtuple("ModelEvaluationArtifact",
+                                     ["evaluated_model_path","is_model_accepted"])
+
+ModelPusherArtifact = namedtuple("ModelPusherArtifact",
+                                 ["export_model_file_path","is_model_pusher"])
--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -1,28 +1,24 @@
 
+from collections import namedtuple
 
-training_pipeline_config:
-    pipeline_name: housing
-    artifact_dir: artifact
-    
-data_ingestion_config:
-    dataset_download_url: https://raw.githubusercontent.com/ageron/handson-ml/master/datasets/housing/housing.tgz
-    raw_data_dir: raw_data
-    tgz_download_dir: tgz_data
-    ingested_dir: ingested_data
-    ingested_train_dir: train
-    ingested_test_dir: test
+DataIngestionConfig = namedtuple("DataIngestionConfig",
+                    ["dataset_download_url","tgz_download_dir","raw_data_dir",
+                     "ingested_train_dir","ingested_test_dir"])
 
-data_validation_config:
-    
-    
-data_transformation_config:
-    
+DataValidationConfig = namedtuple("DataValidationConfig",
+                                  ["schema_file_path","report_file_path",
+                                   "report_page_file_path"])
 
-model_trainer_config:
-    
-model_evaluation_config:
-    
-model_pusher_config:
-    model_export_dir : saved_models
-    
-    
+DataTransformationConfig = namedtuple("DataTransformationConfig", 
+                                      ["add_bedroom_per_room","transformed_train_dir",
+                                       "transformed_test_dir",
+                                       "preprocessed_object_file_path"])
+
+ModelTrainerConfig = namedtuple("ModelTrainerConfig", 
+                                ["trained_model_file_path",
+                                "base_accuracy","model_config_file_path" ])
+
+ModelEvaluationConfig = namedtuple("ModelEvaluationConfig", 
+                                   ["model_evaluation_file_path", "time_stamp"])
+
+ModelPusherConfig = namedtuple("ModelPusherConfig",["export_dir_path"])
